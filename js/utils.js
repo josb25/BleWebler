7d3f5958968f99b021a41216068d029fc0501908
josb25 <joh.schmidt.bruecken@gmail.com>
2025-10-06 19:41:59 +0200

    Added experimental support for P15 after to user feedback. I couldn't confirm function because I don't own a P15

--- js/utils.js
+++ js/utils.js
@@ -8,6 +8,16 @@ const supportedPrinters = [
     optionalServices: ["0000ff00-0000-1000-8000-00805f9b34fb"], // UUIDs needed
     px: 96 //printed width in px
   },
+
+  { 
+    name: "Marklife_P15",
+    namePrefix: "P15_", //prefix to search for in the name while connecting via BLE
+    pattern: /^P15_.+?_BLE$/, //Pattern used to distinguish this printer from others after connecting via BLE
+    handler: printLabelOnP12, // function called to print a label -> Parameters: bluetooth device and bitmap; uses the same function as P12 as it seems to work
+    optionalServices: ["0000ff00-0000-1000-8000-00805f9b34fb"], // UUIDs needed
+    px: 96 //printed width in px
+  },
+
   // Add more printers here
 ];
 
