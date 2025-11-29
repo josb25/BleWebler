243b6136b2e2983685cbe9667c6c19a219f23afd
josb25 <joh.schmidt.bruecken@gmail.com>
2025-11-29 23:37:30 +0100

    added dpm value

--- js/utils.js
+++ js/utils.js
@@ -6,7 +6,8 @@ const supportedPrinters = [
     pattern: /^P12_.+?_BLE$/, //Pattern used to distinguish this printer from others after connecting via BLE
     handler: printLabelOnP12, // function called to print a label -> Parameters: bluetooth device and bitmap
     optionalServices: ["0000ff00-0000-1000-8000-00805f9b34fb"], // UUIDs needed
-    px: 96 //printed width in px
+    px: 96, //printed width in px
+    dpm: 8 //printer dots per mm (203 dpi)
   },
 
   { 
@@ -15,7 +16,8 @@ const supportedPrinters = [
     pattern: /^P15_.+?_BLE$/, //Pattern used to distinguish this printer from others after connecting via BLE
     handler: printLabelOnP12, // function called to print a label -> Parameters: bluetooth device and bitmap; uses the same function as P12 as it seems to work
     optionalServices: ["0000ff00-0000-1000-8000-00805f9b34fb"], // UUIDs needed
-    px: 96 //printed width in px
+    px: 96, //printed width in px
+    dpm: 8 //printer dots per mm (203 dpi)
   },
 
   // Add more printers here
